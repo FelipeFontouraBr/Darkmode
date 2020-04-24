046d1934e5de551d58cae0aa110239302918fd70
Felipe Fontoura <felipefontouramec@gmail.com>
2020-04-24 12:35:20 +0200

    Created object to get html properties
    Created object to get html properties

--- Darkmode/script.js
+++ Darkmode/script.js
@@ -7,11 +7,11 @@ const getStyle = (element, style) =>
         .getComputedStyle(element)
         .getPropertyValue(style)
 
-const initialColors = { //vamos pegar as cores iniciais
+const initialColors = { //vamos pegar as cores iniciais //Objeto com as propriedades exatas do html
     bg: getStyle(html, "--bg"), //colocamos o html e queremos dele o bg(background)
-    bg: getStyle(html, "--bg"),
-    bg: getStyle(html, "--bg"),
-    bg: getStyle(html, "--bg"),
+    bgPanel: getStyle(html, "--bg-panel"),
+    colorHeadings: getStyle(html, "--color-headings"),
+    colorText: getStyle(html, "--color-text"),
 }  
 
 const changeColors = (colors) => { //um função que recebe colors //aqui precisamos do dark modes e initial mode (cor inicial)
