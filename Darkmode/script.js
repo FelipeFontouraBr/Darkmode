27498e44c87a03c49019206f41a23e5b62a0ceb0
Felipe Fontoura <felipefontouramec@gmail.com>
2020-04-24 12:24:15 +0200

    Constant to get the html style
    created the constant to get the html style for initial colors

--- Darkmode/script.js
+++ Darkmode/script.js
@@ -1,7 +1,11 @@
 const html = document.querySelector("html")
 const checkbox = document.querySelector("input[name=theme]")//Checkbox responsável por fazer a alteração, toda mudança vamos aplicar algo
 
-const changeColors = (colors) => { //um função que recebe colors
+const initialColors = { //vamos pegar as cores iniciais
+    bg: window.getComputedStyle(html).getPropertyValue("--bg")//pegar esse estilo do css
+}  
+
+const changeColors = (colors) => { //um função que recebe colors //aqui precisamos do dark modes e initial mode (cor inicial)
 
 }
 
